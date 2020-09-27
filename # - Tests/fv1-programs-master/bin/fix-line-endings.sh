bb7e5fb38e1cdc9a28a025e30c4f8cb2f2f87f27
effectspcbs <yann.gaudineau@gmail.com>
2020-09-27 11:03:50 +0200

    20200927/11:03

--- # - Tests/fv1-programs-master/bin/fix-line-endings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#for file in `find ./static -type f -name "*.spn" | grep -v "static/files/spinsemi/" | grep -v "static/files/slacker-bf/"`; do
-for file in `find ./static -type f -name "*.spn"`; do
-  file $file | grep -v CRLF
-  if [ $? == 0 ]; then
-    perl -pi -e 's/\n/\r\n/' $file
-  fi
-done
--- # - Tests/fv1-programs-master/static/scripts/mailerlite.js
+++
@@ -1,1 +0,0 @@
-      function ml_webform_success_1741186(){var r=ml_jQuery||jQuery;r(".ml-subscribe-form-1741186 .row-success").show(),r(".ml-subscribe-form-1741186 .row-form").hide()}
